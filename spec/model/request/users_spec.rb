518c71c74123c01e823c4718b137b9fdce3af995
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-01 15:42:00 +0200

    Add rspec

---
+++ spec/model/request/users_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :request do
+  describe 'GET #index' do
+    before(:example) do
+      get '/users'
+    end
+
+    it 'response status was correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "correct 'index' template rendered" do
+      expect(response).to render_template('index')
+    end
+
+    it "response body includes correct placeholder text, 'List of all Users'" do
+      expect(response.body).to include('List of all Users')
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) do
+      get '/users/175'
+    end
+
+    it 'response status was correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'correct template was rendered' do
+      expect(response).to render_template(:show)
+    end
+
+    it "response body includes correct placeholder text, 'User Details'" do
+      expect(response.body).to include('User Details')
+    end
+  end
+end
