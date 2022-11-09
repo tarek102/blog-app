88ff3fd312ef560f746d44eba8a1fcec88bdd3a1
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-09 13:02:52 +0500

    Add test cases for the User index page

---
+++ spec/view/user_index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+RSpec.describe 'User index', type: :feature do
+  before(:example) do
+    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', post_counter: 1)
+    visit users_path
+  end
+  describe 'GET /users' do
+    it 'display the correct username' do
+      expect(page).to have_content('Tom')
+    end
+    it 'display the correct number of post' do
+      expect(page).to have_content(@user.post_counter)
+    end
+    it 'display the correct photo' do
+      expect(page.html).to include(@user.photo)
+    end
+    it 'makes sure that the user is redirected to that user\'s show page' do
+      expect(page).to have_link('Tom', href: user_path(@user))
+    end
+  end
+end
