5f52534a24bb37c75d9dcac86d9bdf82d4edbc7a
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-10 14:56:09 +0200

    add classes and fix signup and login

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,10 +15,14 @@ class PostsController < ApplicationController
   # skip_before_action :verify_authenticity_token
 
   def create
-    @post = Post.new(post_params)
+    # @post = Post.new(post_params)
     # @post = Post.new(title: post_params[:title], text: post_params[:text], author: current_user)
-    @post.author = current_user
-    if @post.save
+    # @post.author = current_user
+    new_post = current_user.posts.new(post_params)
+    new_post.likes_counter = 0
+    new_post.comments_counter = 0
+
+    if new_post.save
       redirect_to user_posts_path(current_user)
     else
       render :new, status: :unprocessable_entity
