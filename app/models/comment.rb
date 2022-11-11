c4e748d0139c3bd18e6585ed5d71a88c7879b5f1
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-11 10:35:56 +0200

    decrease comments counter after destroy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,15 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
   after_save :increase_comment_counter
+  after_destroy :decrease_comments_counter
 
   def increase_comment_counter
     post.increment!(:comments_counter)
+  end
+
+  def decrease_comments_counter
+    post.decrement!(:comments_counter)
   end
 end
 2
