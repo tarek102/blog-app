d70714d48f1e50d8bf6bb23db563218868968c4e
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-11 10:44:13 +0200

    Add foreign key and decrease_posts_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,25 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_save :update_posts_counter
+  after_destroy :decrease_posts_counter
+  after_initialize :init
 
   def return_recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  def decrease_posts_counter
+    author.decrement!(:posts_counter)
+  end
+
+  def init
+    self.comments_counter ||= 0
+    self.likes_counter ||= 0
   end
 
   private
