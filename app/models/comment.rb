dce0025a8b299bf65b416438f0b362123988fd1f
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-11 10:37:29 +0200

    update comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,13 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, foreign_key: 'post_id', class_name: 'Post'
   after_save :increase_comment_counter
-  after_destroy :decrease_comments_counter
+  after_destroy :decrease_comment_counter
 
   def increase_comment_counter
     post.increment!(:comments_counter)
   end
 
-  def decrease_comments_counter
+  def decrease_comment_counter
     post.decrement!(:comments_counter)
   end
 end
